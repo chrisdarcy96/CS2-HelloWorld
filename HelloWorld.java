d935b9337ec8f0493f87bde4867c274452eee1dd
chrisdarcy96 <chrisdarcy96@gmail.com>
2015-05-06 08:53:17 -0700

    Added user input.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class HelloWorld{
+  private static Scanner in=new Scanner(System.in);
   public static void main(String[] args){
-    System.out.println("Hello World!");
-    System.out.println("Goodbye");
+    System.out.print("What is your name?");
+    String name=in.nextLine();
+    System.out.println("Hello "+name+"!");
   }
 
 }
